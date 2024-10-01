a4c423e2119733e36f4b7828d302f4db8aee038c
sakunravisanka <sakunravisanka1@gmail.com>
2024-10-01 23:16:52 +0530

    BBbMS

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+
+
+ 
+  <footer class="py-4" style="background-color: #212529; color: #fff;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <h5 class="text-uppercase">BloodBank & Donor</h5>
+                <p>Your reliable source for blood donation and recipient services.</p>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-4">
+                <h5 class="text-uppercase">Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="page.php?type=aboutus" style="color: #00bcd4; text-decoration: none;">About US</a></li>
+                    <li><a href="page.php?type=donor" style="color: #00bcd4; text-decoration: none;">Donor</a></li>
+                    <li><a href="become-donar.php" style="color: #00bcd4; text-decoration: none;">Become Donor</a></li>
+                    <li><a href="search-donor.php" style="color: #00bcd4; text-decoration: none;">Search Donor</a></li>
+                    <li><a href="contact.php" style="color: #00bcd4; text-decoration: none;">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-4">
+                <h5 class="text-uppercase">Get Involved</h5>
+                <ul class="list-unstyled">
+                    <li><a href="become-donar.php" style="color: #00bcd4; text-decoration: none;">Become a Donor</a></li>
+                    <li><a href="volunteer.php" style="color: #00bcd4; text-decoration: none;">Volunteer</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <p>
+                    Email: <a href="sakunravisanka1@gmail.com" style="color: #00bcd4;">sakunravisanka1@gmail.com</a><br>
+                    Phone: 0789619294
+                </p>
+               
+            </div>
+        </div>
+     
+    </div>
+</footer>
